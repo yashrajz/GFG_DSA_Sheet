68e435a33677649be4107fd835bb298a70bf2a47
yashrajz <yashrajz.me@gmail.com>
2026-02-19 15:14:42 +0530

    Add implementation for checking if two strings are anagrams

---
+++ Anagram.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+public class Anagram {
+   
+    public static boolean areAnagrams(String s1, String s2) {
+        
+        //checking the length of both string
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        //freq counting +1 in hashMap
+        for(char ch : s1.toCharArray()){
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        }
+        
+        //freq counting -1 in hashMap
+        for(char ch : s2.toCharArray()){
+            hm.put(ch, hm.getOrDefault(ch, 0) - 1);
+        }
+
+        for(int val : hm.values()){
+            if(val != 0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
