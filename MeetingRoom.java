66d7e458c54f18b65449a4e75a9970b8314f1f17
yashrajz <yashrajz.me@gmail.com>
2026-02-25 14:06:05 +0530

    Add implementation for checking if all meetings can be attended without overlap

---
+++ MeetingRoom.java
@@ -0,0 +1,22 @@
+//www.geeksforgeeks.org/batch/skill-up-dsa/track/su-dsa-topic-wise-week5/problem/attend-all-meetings
+
+import java.util.Arrays;
+
+public class MeetingRoom {
+
+    static boolean canAttend(int[][] arr) {
+        
+        int n = arr.length;
+        
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        for (int i = 0; i < n - 1; i++) {
+            
+            // Compare the current meeting's end time with the 
+            // next meeting's start time to check for overlap
+            if (arr[i][1] > arr[i + 1][0])
+                return false;
+        }
+        return true;
+    }
+}
