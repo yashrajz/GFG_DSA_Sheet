87634b9686369fd6e9360f9a16cded443b1b94af
yashrajz <yashrajz.me@gmail.com>
2026-02-28 18:05:43 +0530

    Add implementation for finding H-Index from citation counts

---
+++ FindHIndex.java
@@ -0,0 +1,33 @@
+//www.geeksforgeeks.org/batch/gfg-160-problems/track/sorting-gfg-160/problem/find-h-index--165609
+
+
+public class FindHIndex {
+    
+    public int hIndex(int[] citations) {
+        
+        int n = citations.length;
+        int[] freq = new int[n + 1];
+
+        // Count the frequency of citations
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n)
+                freq[n] += 1;
+            else
+                freq[citations[i]] += 1;
+        }
+
+        int idx = n;
+        
+        // variable to keep track of the count of papers
+        // having at least idx citations
+        int s = freq[n]; 
+        while (s < idx) {
+            idx--;
+            s += freq[idx];
+        }
+        
+        // return the largest index for which the count of 
+        // papers with at least idx citations becomes >= idx
+        return idx;
+    }
+}
