93a915c1e581bbf522c5126846f8332343c2fd80
yashrajz <yashrajz.me@gmail.com>
2026-04-15 00:34:38 +0530

    Add implementation for rearranging linked list into odd and even nodes

---
+++ OddEvenLinkedList.java
@@ -0,0 +1,34 @@
+public class OddEvenLinkedList {
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode temp1 = head;
+        ListNode temp2 = head.next;
+        ListNode head2 = head.next;
+
+
+        while(temp2 != null && temp2.next != null){
+            temp1.next = temp1.next.next;
+            temp1 = temp1.next;
+            temp2.next = temp2.next.next;
+            temp2 = temp2.next;
+        }
+            temp1.next = head2;
+
+        return head;
+    }
+    
+    
+}
