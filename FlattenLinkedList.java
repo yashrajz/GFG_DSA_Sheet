a89d50c742549115b57e81713a488b8a1ac262c5
yashrajz <yashrajz.me@gmail.com>
2026-05-05 20:29:30 +0530

    Add implementation for flattening a linked list

---
+++ FlattenLinkedList.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FlattenLinkedList {
+    
+    class Node {
+        int data;
+        Node next;
+        Node bottom;
+
+        Node(int x) {
+            data = x;
+            next = null;
+            bottom = null;
+        }
+    }
+
+    public Node flatten(Node root) {
+        
+        // create an array, store all value, sort it and create new
+        // LinkedList out of it
+        
+        List<Integer> li = new ArrayList<>();
+        
+        while(root != null){
+            
+            li.add(root.data);
+            
+            Node temp = root.bottom;
+            while(temp != null){
+                li.add(temp.data);
+                temp = temp.bottom;
+            }
+            
+            root = root.next;
+            
+        }
+        
+        //sorting part;
+        Collections.sort(li);
+        
+        //now creating a new Linked List;
+        Node head = new Node(-1);
+        Node curr = head;
+        
+        for(int num : li){
+            curr.bottom = new Node(num);
+            curr = curr.bottom;
+        }
+        
+        return head.bottom;
+        
+        
+        
+    }
+
+}
